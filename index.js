38c34b944e3151e1a38920e3a3f312d2c4d2c69b
anes.ugljesa <anesugljesa@gmail.com>
2022-05-15 21:04:31 +0200

    First CICD test

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ var calc = 2;
 app.get('/', function (req, res) {
  res.send('This is simple node app' + "</br>" + 'The result of 1+1 is: ' + calc);
  res.send('This is the first CICD test');
- res.send('This is the second CICD test');
- res.send('This is the third CICD test');
- res.send('This is the fourth CICD test');
- res.send('This is the fifth CICD test'); 
 });
 app.listen(process.env.PORT || 3000);
  
