e5126d05c8f4fbcfc27ae12f39eba5b2dbb7ca84
anes.ugljesa <anesugljesa@gmail.com>
2022-05-15 13:35:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+ 
+var app = express();
+var calc = 2;
+app.get('/', function (req, res) {
+ res.send('This is simple node app' + "</br>" + 'The result of 1+1 is: ' + calc);
+});
+app.listen(process.env.PORT || 3000);
+ 
+module.exports = app;
