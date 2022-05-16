72e86460628262865c884c675c1a4d63e313beaf
anes.ugljesa <anesugljesa@gmail.com>
2022-05-16 20:10:29 +0200

    Third CICD test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var calc = 2;
 app.get('/', function (req, res) {
  res.send('This is simple node app' + "</br>" + 'The result of 1+1 is: ' + calc);
- res.send('This is the second CICD test');
+ res.send('This is the third CICD test');
 });
 app.listen(process.env.PORT || 3000);
  
