005e0642355ec334166409067473b57b920b802c
anes.ugljesa <anesugljesa@gmail.com>
2022-05-20 00:04:08 +0200

    Simple node app

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express();
 var calc = 2;
 app.get('/', function (req, res) {
- res.send('This is simple node app' + "</br>" + 'The result of 1+1 is: ' + calc);
- res.send('CICD test');
+ res.send('Simple node app' + "</br>" + 'The result of 1+1 is: ' + calc);
 });
 app.listen(process.env.PORT || 3000);
  
